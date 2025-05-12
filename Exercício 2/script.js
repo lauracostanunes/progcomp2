4a9320ff270c97701fb729577c9c8aca3eb30f06
Laura Nunes <lauracostanunes2@gmail.com>
2025-05-12 09:24:05 -0300

    Add files via upload

---
+++ Exercício 2/script.js
@@ -0,0 +1,19 @@
+function lucro() {
+    let preco = 5.0;
+    let custoFixo = 200.0;
+    let pessoas = 120;
+    let lucroMaximo = 0;
+    let melhorPreco = 0;
+    let relatorio = '';
+    while (preco >= 1.0) {
+        let lucro = (preco * pessoas) - custoFixo;
+        relatorio += `Preço: R$ ${preco.toFixed(2)} - Pessoas: ${pessoas} - Lucro: R$ ${lucro.toFixed(2)}\n`;
+        if (lucro > lucroMaximo) {
+            lucroMaximo = lucro;
+            melhorPreco = preco;
+        }
+        preco -= 0.5;
+        pessoas += 26;
+    }
+    alert(`${relatorio}\nLucro máximo: R$ ${lucroMaximo.toFixed(2)} com ingresso a R$ ${melhorPreco.toFixed(2)}`);
+}
