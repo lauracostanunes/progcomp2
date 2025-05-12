551b6a85a9a9580f7e4abff3b07ae7f94a1c661e
Laura Nunes <lauracostanunes2@gmail.com>
2025-05-12 19:58:07 -0300

    Add files via upload

--- Exercício 3/script.js
+++ Exercício 3/script.js
@@ -18,12 +18,12 @@ function idades() {
         else if (idade >= 46 && idade <= 60) {
             faixa4++; }
             else {
-            faixa5++;}
+            faixa5++; }
         total++;
         conta++;
     }
-    let perc1 = (faixa1 / total * 100).toFixed(1);
-    let perc5 = (faixa5 / total * 100).toFixed(1);
+    let perc1 = (faixa1 / 8 * 100).toFixed(1);
+    let perc5 = (faixa5 / 8 * 100).toFixed(1);
     alert(`Total de pessoas: ${total}
     Faixa 1 (até 15 anos): ${faixa1}
     Faixa 2 (16 - 30 anos): ${faixa2}
