e4988615881b11cda9410fc6ce1d2ef6cf6632aa
Laura Nunes <lauracostanunes2@gmail.com>
2025-05-12 20:37:56 -0300

    Add files via upload

--- Exercício 1/script.js
+++ Exercício 1/script.js
@@ -1,19 +1,23 @@
 function grupos() {
+    let grupo = 1
+    let A, B, C, D, AUX
+    while (grupo <=5) {
     let conta = 1
-    let grupo = A, B, C, D, AUX
     A = Number(prompt('Informe o valor de A'))
     B = Number(prompt('Informe o valor de B'))
     C = Number(prompt('Informe o valor de C'))
     D = Number(prompt('Informe o valor de D'))
     while (conta <= 3) {
         if (A>B) {
-            AUX = A; B=A; B=AUX }
+            AUX = A; A=B; B=AUX }
         if (B>C) {
             AUX = B; B=C; C=AUX }
         if (C>D) {
                 AUX = C; C=D; D=AUX }
         conta++
     }
-    alert ('Ordem crescente ${A} ${B} ${C} ${D}')
-    alert ('Ordem decrescente ${D} ${C} ${B} {A}')
+    alert (`Ordem crescente: ${A} ${B} ${C} ${D}`)
+    alert (`Ordem decrescente: ${D} ${C} ${B} ${A}`)
+    grupo++
+}
 }
