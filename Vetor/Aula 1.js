f2768c910d9793ecfe2447edbc31aed482f245b4
Laura Nunes <lauracostanunes2@gmail.com>
2025-06-02 20:12:34 -0300

    Add files via upload

---
+++ Vetor/Aula 1.js
@@ -0,0 +1,8 @@
+let vet = []
+vet.push("São Paulo")
+vet.push("Timão")
+vet.push("Palmeiras")
+vet.push("Santos")
+vet.push("Grêmio")
+vet // Para mostrar todo o conteúdo
+vet.length // Tamanho do vetor
---
+++ Vetor/Exercício 1.js
@@ -0,0 +1,7 @@
+function ex1() {
+    let vet = []
+    for (let i = 0; i < 6; i++) {
+        vet.push(Number(prompt(Informe o ${i+1} número)))
+    }
+    for (let i = 0; i < 6; i++) {
+}
