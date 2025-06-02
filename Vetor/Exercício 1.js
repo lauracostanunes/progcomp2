1975e77114a7c119a15c5e8c36a692705f348fbf
Laura Nunes <lauracostanunes2@gmail.com>
2025-06-02 20:20:16 -0300

    Add files via upload

--- Vetor/Exercício 1.js
+++ Vetor/Exercício 1.js
@@ -1,7 +1,19 @@
 function ex1() {
     let vet = []
     for (let i = 0; i < 6; i++) {
-        vet.push(Number(prompt(Informe o ${i+1} número)))
+        vet.push(Number(prompt(`Informe o ${i+1}º número`)))
     }
+    let par = 0, impar = 0
     for (let i = 0; i < 6; i++) {
+        if (vet[i]%2 == 0) { // elemento é par
+            par++
+        }
+        else {
+            impar++
+        }
+    }
 }
+
+
+// i = posição do vetor
+// v[i] = elemento na posição i
